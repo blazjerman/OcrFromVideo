6b6f51b0713e94ba95648b26ff45dde4d0270c5c
blaz <blaz.jerman5@gmail.com>
2025-02-25 00:55:48 +0100

    Change test code.

--- test.py
+++ test.py
@@ -3,7 +3,9 @@ import pandas as pd
 from main import process_video
 from position import Position
 
-
+import os
+import glob
+import pandas as pd
 
 def create_df_from_tuples(data):
 
@@ -20,11 +22,20 @@ p1 = Position("kohm", 353, 630, 319, 48)
 p2 = Position("N", 853, 435, 79, 36)
 
 
-data = process_video("test.mp4", 1, [p1, p2], ['en'])
+frame_rate = 30
+video_folder = "videos"
+mp4_files = glob.glob(os.path.join(video_folder, "*.mp4"))
 
+for video_path in mp4_files:
 
+    print(f"Proccesing file '{video_path}'.")
 
-df = create_df_from_tuples(data)
-csv_filename = "output.csv"
-df.to_csv(csv_filename, index=False)
-print(f"CSV file '{csv_filename}' has been created!")
+    video_name = os.path.splitext(os.path.basename(video_path))[0]
+    csv_filename = os.path.join(video_folder, f"{video_name}.csv")
+
+    data = process_video(video_path, frame_rate, [p1, p2], ['en'])
+    df = create_df_from_tuples(data)
+    df.to_csv(csv_filename, index=False)
+
+    print(f"CSV file '{csv_filename}' has been created!")
+
